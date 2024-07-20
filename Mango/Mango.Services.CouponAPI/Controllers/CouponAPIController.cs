4f329c921671551727643182b57ddb735b31c3d3
Leger DJIBA <leger.djiba@outlook.fr>
2024-07-20 01:46:58 +0000

    Update Coupon API Controller

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public object Get() 
+        {
+            try
+            {
+                IEnumerable<Coupon> objectList = _db.Coupons.ToList();
+                return objectList;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
+        }
+    }
+}
