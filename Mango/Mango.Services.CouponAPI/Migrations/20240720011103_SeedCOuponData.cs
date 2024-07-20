7ec88c688d416188699112a25a544b0d8f749cd7
Leger DJIBA <leger.djiba@outlook.fr>
2024-07-20 01:31:33 +0000

    First Commit

---
+++ Mango/Mango.Services.CouponAPI/Migrations/20240720011103_SeedCOuponData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCOuponData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "180FF", 10.0, 20 },
+                    { 2, "280FF", 20.0, 40 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+        }
+    }
+}
