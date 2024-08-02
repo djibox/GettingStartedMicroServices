a412713372362f9cb4cec20e2bc2ae34a470e8c0
Léger Djiba <leger.djiba@outlook.fr>
2024-08-02 13:18:26 +0000

    Update

--- Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -78,5 +78,68 @@ namespace Mango.Services.CouponAPI.Controllers
             return _response;
         }
 
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.FirstOrDefault(c=>c.CouponId.Equals(id));
+                if(obj != null)
+                {
+                    _db.Coupons.Remove(obj);
+                    _db.SaveChanges();
+                    _response.Result = _mapper.Map<CouponDto>(obj);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
     }
 }
--- Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0e806e012dca9603613932818931985a5483aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47a9438038f4cdedd08433aeb07347f6c253ae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
