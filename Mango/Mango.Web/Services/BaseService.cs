4b159b22a85b37215445c94e20d86cfcd97fa7bb
Léger Djiba <leger.djiba@outlook.fr>
2024-08-08 14:05:01 +0000

    Update

--- Mango/Mango.Web/Services/BaseService.cs
+++ Mango/Mango.Web/Services/BaseService.cs
@@ -1,5 +1,8 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Services.IService;
+using System.Text;
+using newton.json;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Services
 {
@@ -12,7 +15,30 @@ namespace Mango.Web.Services
         }
         public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
         {
-            throw new NotImplementedException();
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(requestDto.Url);
+            if (requestDto.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+            }
+            HttpResponseMessage?apiResponse =null;
+            switch (requestDto.ApiType)
+            {
+                case Utility.SD.ApiType.GET:
+                    message.Method = HttpMethod.Get;
+                    break;
+                case Utility.SD.ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case Utility.SD.ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case Utility.SD.ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+            }
         }
     }
 }
--- Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ff58111ca70bdae14e3053aeece9e22465be29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622c0158165310b7f0117a74811e97265370cbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
