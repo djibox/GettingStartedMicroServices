622c0158165310b7f0117a74811e97265370cbba
Léger Djiba <leger.djiba@outlook.fr>
2024-08-08 13:42:25 +0000

    Update Data

--- Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a412713372362f9cb4cec20e2bc2ae34a470e8c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ff58111ca70bdae14e3053aeece9e22465be29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Mango/Mango.Web/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IService;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
